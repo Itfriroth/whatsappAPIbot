aae56ef7b63e44cb69dcfbd79d6b0e4dde91e64b
Itfriroth <78076229+Itfriroth@users.noreply.github.com>
2025-04-11 19:04:33 -0400

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export defaul {
+export default {
     WEBHOOK_VERIFY_TOKEN:process.env.WEBHOOK_VERIFY_TOKEN,
     API_TOKEN: process.env.API_TOKEN,
     BUSINESS_PHONE: process.env.BUSINESS_PHONE,
